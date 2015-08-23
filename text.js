1b48b728687e4773e24ef5ab161b489e9d9dd710
Sarah Edkins <edkins.sarah@gmail.com>
2015-08-23 19:05:09 -0400

    Color/graphic changes for win/loss

---
+++ text.js
@@ -0,0 +1,9 @@
+var won_msg = "Congratulations, you won!!";
+var lost_msg = "Game over. Better luck next time.";
+
+var warm_msg = "You're getting warmer.";
+var hot_msg = "It's getting hot in here.";
+var chilly_msg = "Brr, chilly.";
+var cold_msg = "Colder than a snowcone.";
+
+
